ee6114d376a43bddff07629d5749a23ea2884dfe
MarianBastiurea <marian.bastiurea@gmail.com>
2024-07-26 07:17:53 +0100

    create new classes using @Component

---
+++ src/main/java/com/marianbastiurea/learnspringframework/c04example/BusinessCalculationService.java
@@ -0,0 +1,21 @@
+package com.marianbastiurea.learnspringframework.c04example;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BusinessCalculationService {
+
+    private DataService dataService;
+
+    public BusinessCalculationService(DataService dataService) {
+        super();
+        this.dataService = dataService;
+    }
+
+    public int findMax() {
+        return Arrays.stream(dataService.retrieveData()).max().orElse(0);
+    }
+
+}
