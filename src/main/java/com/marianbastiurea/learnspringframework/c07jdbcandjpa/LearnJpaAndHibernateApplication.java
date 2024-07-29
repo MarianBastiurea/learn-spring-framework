5be165be70479523468d3c06cc0541505a2af1dd
MarianBastiurea <marian.bastiurea@gmail.com>
2024-07-29 14:34:31 +0100

    create new classes using JPA and Hibernate

---
+++ src/main/java/com/marianbastiurea/learnspringframework/c07jdbcandjpa/LearnJpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.marianbastiurea.learnspringframework.c07jdbcandjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJpaAndHibernateApplication.class, args);
+    }
+
+}
