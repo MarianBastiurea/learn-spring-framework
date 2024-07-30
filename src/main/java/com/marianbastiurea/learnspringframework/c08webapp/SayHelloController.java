91f7776c20903055c2961a9dbdbf8d15bce7a662
MarianBastiurea <marian.bastiurea@gmail.com>
2024-07-30 13:40:17 +0100

    update pom.xml and create SayHello class for web app

--- src/main/java/com/marianbastiurea/learnspringframework/c08webapp/SayHelloController.java
+++ src/main/java/com/marianbastiurea/learnspringframework/c08webapp/SayHelloController.java
@@ -10,11 +10,28 @@ public class SayHelloController {
     //"say-hello" => "Hello! What are you learning today?"
 
     //say-hello
-    // http://localhost:8081/say-hello
+    // http://localhost:8080/say-hello
     @RequestMapping("say-hello")
     @ResponseBody
     public String sayHello() {
         return "Hello! What are you learning today?";
+    }
+
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> My First HTML Page - Changed</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My first html page with body - Changed");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
     }
 
 }
