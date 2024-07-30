c80d9f8c35a43d09e3e24b868f3cc8b20bc476d3
MarianBastiurea <marian.bastiurea@gmail.com>
2024-07-30 13:52:52 +0100

    create a login class in web app

---
+++ src/main/java/com/marianbastiurea/learnspringframework/c08webapp/LoginController.java
@@ -0,0 +1,15 @@
+package com.marianbastiurea.learnspringframework.c08webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    ///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
+
+    @RequestMapping("login")
+    public String gotoLoginPage() {
+        return "login";
+    }
+}
