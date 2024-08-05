95ab57ba65d9c81ab19a4e5cdac4b1eb963e7d21
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-05 16:12:36 +0100

    update algorithm for UserResource and create UserNotFoundException class

--- src/main/java/com/marianbastiurea/learnspringframework/c09restfulwebservices/UserResource.java
+++ src/main/java/com/marianbastiurea/learnspringframework/c09restfulwebservices/UserResource.java
@@ -1,7 +1,14 @@
 package com.marianbastiurea.learnspringframework.c09restfulwebservices;
+import java.net.URI;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -12,20 +19,33 @@ public class UserResource {
         this.service = service;
     }
 
-    // GET /users
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
-     //GET /users
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if(user==null)
+            throw new UserNotFoundException("id:"+id);
+
+        return user;
     }
-    //POST /users
+
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
+
+
 }
