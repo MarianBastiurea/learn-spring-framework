6d5d28cbd32f7d4fe94e575bd5ae4a9da1f8d352
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-05 16:17:55 +0100

    update algorithm

--- src/main/java/com/marianbastiurea/learnspringframework/c09restfulwebservices/UserDaoService.java
+++ src/main/java/com/marianbastiurea/learnspringframework/c09restfulwebservices/UserDaoService.java
@@ -35,5 +35,8 @@ public class UserDaoService {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().get();
     }
-
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
 }
