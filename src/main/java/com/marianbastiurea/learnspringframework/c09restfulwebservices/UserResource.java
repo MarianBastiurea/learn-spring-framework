e6f9ec4cc3f2647fb4f25e09664806dbe4b4350b
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-08 12:09:39 +0100

    using static and dynamic filtering

--- src/main/java/com/marianbastiurea/learnspringframework/c09restfulwebservices/UserResource.java
+++ src/main/java/com/marianbastiurea/learnspringframework/c09restfulwebservices/UserResource.java
@@ -1,11 +1,21 @@
 package com.marianbastiurea.learnspringframework.c09restfulwebservices;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import java.net.URI;
 import java.util.List;
 
-import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserResource {
@@ -21,14 +31,25 @@ public class UserResource {
         return service.findAll();
     }
 
+
+    //http://localhost:8080/users
+
+    //EntityModel
+    //WebMvcLinkBuilder
+
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if(user==null)
             throw new UserNotFoundException("id:"+id);
 
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder link =  linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
     }
 
     @DeleteMapping("/users/{id}")
