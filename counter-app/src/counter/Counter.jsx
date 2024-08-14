a875462f703aec093986a2a82177ac04c9ed5ebf
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-14 16:00:44 +0100

    create CounterButton

--- counter-app/src/counter/Counter.jsx
+++ counter-app/src/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import {PropTypes} from 'prop-types'
 import './Counter.css'
+import CounterButton from './CounterButton'
 
 export default function Counter() {
     
@@ -12,6 +12,10 @@ export default function Counter() {
 
     function decrementCounterParentFunction(by) {
         setCount(count - by)
+    }
+
+    function resetCounter() {
+        setCount(0)
     }
 
     return (
@@ -26,45 +30,9 @@ export default function Counter() {
             <CounterButton by={5} 
                 incrementMethod={incrementCounterParentFunction}
                 decrementMethod={decrementCounterParentFunction}/>
+            <button className="resetButton" 
+                        onClick={resetCounter}
+                >Reset</button>
         </>
     )
-}
-
-function CounterButton({by, incrementMethod, decrementMethod}) {    
-
-    //[0, ƒ]
-    //const [firstElt, secondElt] = array
-    const [count, setCount] = useState(0)
-
-    function incrementCounterFunction() {
-        setCount(count + by)
-        incrementMethod(by)
-    }
-
-    function decrementCounterFunction() {
-        setCount(count - by)
-        decrementMethod(by)
-    }
-
-    return (
-        <div className="Counter">
-            <div>
-                <button className="counterButton" 
-                        onClick={incrementCounterFunction}
-                >+{by}</button>
-                <button className="counterButton" 
-                        onClick={decrementCounterFunction}
-                >-{by}</button>
- 
-            </div>
-        </div>
-    )
-}
-
-CounterButton.propTypes = {
-    by: PropTypes.number
-}
-
-CounterButton.defaultProps = {
-    by: 5
 }
