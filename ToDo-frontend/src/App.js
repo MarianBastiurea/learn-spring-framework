ff88cf0bb51a61c75f91dc0c364f176c403b0abb
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-14 09:56:31 +0100

    changes in folder structure

--- ToDo-frontend/src/App.js
+++ ToDo-frontend/src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
-import Counter from './components/counter/Counter'
+import LearningComponent from './components/learning-examples/LearningComponent'
+
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <LearningComponent />
     </div>
   )
 }
-
-//{property1: 'value1', property2: 'value2'}
-// function PlayingWithProps(properties) {
-  
-//   console.log(properties)
-//   console.log(properties.property1)
-//   console.log(properties.property2)
-  
-//   return (
-//     <div>Props</div>
-//   )
-// }
-
-function PlayingWithProps( {property1, property2} ) {
-  console.log(property1)
-  console.log(property2)
-  
-  return (
-    <div>Props</div>
-  )
-}
-
 
 export default App;
