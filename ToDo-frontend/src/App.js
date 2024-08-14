bd1046eb3025fb5b569fe79413552f89400d74d4
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-14 08:32:02 +0100

    create counter app

--- ToDo-frontend/src/App.js
+++ ToDo-frontend/src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
-import FirstComponent from './components/learning-examples/FirstComponent'
-import SecondComponent from './components/learning-examples/SecondComponent'
-import ThirdComponent from './components/learning-examples/ThirdComponent'
-import FourthComponent from './components/learning-examples/FourthComponent'
+import Counter from './components/counter/Counter'
 function App() {
   return (
     <div className="App">
-     <FirstComponent></FirstComponent>
-     <SecondComponent></SecondComponent>
-     <ThirdComponent></ThirdComponent>
-     <FourthComponent></FourthComponent>
+      <Counter/>
     </div>
-  );
+  )
 }
+
+//{property1: 'value1', property2: 'value2'}
+// function PlayingWithProps(properties) {
+  
+//   console.log(properties)
+//   console.log(properties.property1)
+//   console.log(properties.property2)
+  
+//   return (
+//     <div>Props</div>
+//   )
+// }
+
+function PlayingWithProps( {property1, property2} ) {
+  console.log(property1)
+  console.log(property2)
+  
+  return (
+    <div>Props</div>
+  )
+}
+
+
 export default App;
