62aed0b1ce52ef9bf147195b35d8da21314f8684
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-16 21:10:54 +0100

    sharing react state with multiple components using auth context

--- ToDo-app/src/components/todo/ToDoApp.jsx
+++ ToDo-app/src/components/todo/ToDoApp.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
-import './TodoApp.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LogoutComponent from './LogoutComponent'
-import FooterComponent from './FooterComponent'
 import HeaderComponent from './HeaderComponent'
 import ListTodosComponent from './ListTodosComponent'
 import ErrorComponent from './ErrorComponent'
-import WelcomeComponent from'./WelcomeComponent'
+import WelcomeComponent from './WelcomeComponent'
 import LoginComponent from './LoginComponent'
+import AuthProvider from './security/AuthContext'
+
+import './TodoApp.css'
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-                      
-            
-            <BrowserRouter>
-            <HeaderComponent /> 
-                <Routes>
-                    <Route path='/' element={ <LoginComponent /> } />
-                    <Route path='/login' element={ <LoginComponent /> } />
-                    <Route path='/welcome/:username' element={<WelcomeComponent /> } />
-                    <Route path='/todos' element={<ListTodosComponent /> } />
-                    <Route path='/logout' element={<LogoutComponent /> } />
-                    
-                    <Route path='*' element={<ErrorComponent /> } />
+            <AuthProvider>
+                <BrowserRouter>
+                    <HeaderComponent />
+                    <Routes>
+                        <Route path='/' element={ <LoginComponent /> } />
+                        <Route path='/login' element={ <LoginComponent /> } />
+                        <Route path='/welcome/:username' element={<WelcomeComponent /> } />
+                        <Route path='/todos' element={<ListTodosComponent /> } />
+                        <Route path='/logout' element={<LogoutComponent /> } />
+                        
+                        <Route path='*' element={<ErrorComponent /> } />
 
-                </Routes>
-            </BrowserRouter>
-            
-            
-            
+                    </Routes>
+                </BrowserRouter>
+            </AuthProvider>
         </div>
     )
 }
