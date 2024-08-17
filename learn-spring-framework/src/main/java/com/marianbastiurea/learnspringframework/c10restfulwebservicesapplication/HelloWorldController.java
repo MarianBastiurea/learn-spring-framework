b81116dc5cdf4673f6c212114a6f6d0a6d9fc869
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-17 16:11:53 +0100

    create package restfulwebservice

--- learn-spring-framework/src/main/java/com/marianbastiurea/learnspringframework/c10restfulwebservicesapplication/HelloWorldController.java
+++ learn-spring-framework/src/main/java/com/marianbastiurea/learnspringframework/c10restfulwebservicesapplication/HelloWorldController.java
@@ -1,11 +1,13 @@
-package com.marianbastiurea.learnspringframework.c09restfulwebservices;
+package com.marianbastiurea.learnspringframework.c10restfulwebservicesapplication;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorld {
+public class HelloWorldController {
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -21,3 +23,4 @@ public class HelloWorld {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
+
