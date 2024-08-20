5344d79efa5cc9cffdc115277520c7ba78a50b5e
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-20 06:07:15 +0100

    update algorithm

--- ToDo-app/src/components/todo/WelcomeComponent.jsx
+++ ToDo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,10 +1,33 @@
 import {useParams, Link} from 'react-router-dom'
+import { useState } from 'react'
+import { retrieveHelloWorldPathVariable } from './api/HelloWorldApiService'
 
 function WelcomeComponent() {
 
     const {username } = useParams()
 
-    console.log(username)
+    const [message, setMessage] = useState(null)
+
+    function callHelloWorldRestApi(){
+        console.log('called')
+              
+        retrieveHelloWorldPathVariable('Ranga')
+            .then( (response) => successfulResponse(response) )
+            .catch ( (error) => errorResponse(error) )
+            .finally ( () => console.log('cleanup') )
+
+    }
+
+    function successfulResponse(response) {
+        console.log(response)
+        //setMessage(response.data)
+        setMessage(response.data.message)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
+
 
     return (
         <div className="WelcomeComponent">
@@ -12,7 +35,13 @@ function WelcomeComponent() {
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>
             </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+                    Call Hello World</button>
+            </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
+
 export default WelcomeComponent
