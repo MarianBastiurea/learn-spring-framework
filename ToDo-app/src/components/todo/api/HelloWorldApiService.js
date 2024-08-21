f70326bcd8495bdfc71c2f55ebba640fbf51e242
MarianBastiurea <marian.bastiurea@gmail.com>
2024-08-21 15:17:56 +0100

    update security algorithm

--- ToDo-app/src/components/todo/api/HelloWorldApiService.js
+++ ToDo-app/src/components/todo/api/HelloWorldApiService.js
@@ -1,14 +1,8 @@
-import axios from 'axios'
+import { apiClient } from './ApiClient'
 
 // export function retrieveHelloWorldBean(){
 //     return axios.get('http://localhost:8080/hello-world-bean')
 // }
-
-const apiClient = axios.create(
-    {
-        baseURL: 'http://localhost:8080'
-    }
-);
 
 export const retrieveHelloWorldBean 
     = () => apiClient.get('/hello-world-bean')
@@ -16,18 +10,20 @@ export const retrieveHelloWorldBean
 
 //Response to preflight request doesn't pass access control check => Authorization header
 export const retrieveHelloWorldPathVariable
-    = (username) => apiClient.get(`/hello-world/path-variable/${username}`,{
-        headers: {
-            Authorization: 'Basic aW4yOG1pbnV0ZXM6ZHVtbXk='
-        }
-    })
-
-export const executeBasicAuthenticationService
-    = (token) => apiClient.get(`/basicauth`,{
+    = (username, token) => apiClient.get(`/hello-world/path-variable/${username}`,{
         headers: {
             Authorization: token
         }
     })
+
+export const executeBasicAuthenticationService
+    = (token) => apiClient.get(`/basicauth`
+    // ,{
+    //     headers: {
+    //         Authorization: token
+    //     }
+    // }
+    )
 
 
     
